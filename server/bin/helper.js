1a531c4e527fe132e5dc6e782d8d4928d61dd215
moksh <muktimoksh23@gmail.com>
2026-09-25 15:17:29 +0530

    setup prisma and express

---
+++ server/bin/helper.js
@@ -0,0 +1,49 @@
+const debug = require("debug")("server:server");
+
+function onError(error, port) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      
+    default:
+      throw error;
+  }
+}
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onListening(server) {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
+
+module.exports = {
+  normalizePort,
+  onError,
+  onListening,
+};
